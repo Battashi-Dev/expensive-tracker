9a121d52176421cefe8ab89f25af697d8b5fc319
Battashi <battashi.dev@gmail.com>
2026-06-02 20:45:10 +0400

    Add category filtering logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,26 @@ import { useState } from "react";
 import ExpensiveFilter from "./Components/ExpensiveFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "Food", amount: 50, category: "Grocery" },
     { id: 2, description: "Water", amount: 1000, category: "Utilities" },
     { id: 4, description: "movie", amount: 200, category: "Entertainment" },
   ]);
 
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((expense) => expense.category === selectedCategory)
+    : expenses;
   return (
-    <Grid>
-      <GridItem p={3}>
-        <ExpensiveFilter />
+    <Grid p={3}>
+      <GridItem>
+        <ExpensiveFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </GridItem>
+      <GridItem>
         <ExpensiveList
-          expenses={expenses}
+          expenses={visibleExpenses}
           onDelete={(id) =>
             setExpenses(expenses.filter((expense) => expense.id !== id))
           }
--- src/Components/ExpensiveFilter.tsx
+++ src/Components/ExpensiveFilter.tsx
@@ -1,8 +1,15 @@
 import { Select } from "@chakra-ui/react";
 import { categories } from "./constant.ts";
-const ExpensiveFilter = () => {
+
+interface Props {
+  onSelectCategory: (category: string) => void;
+}
+const ExpensiveFilter = ({ onSelectCategory }: Props) => {
   return (
-    <Select placeholder="All Categories">
+    <Select
+      placeholder="All Categories"
+      onChange={(event) => onSelectCategory(event.target.value)}
+    >
       {categories.map((category) => (
         <option key={category}>{category}</option>
       ))}
